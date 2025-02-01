3e6a21ea2726c5e76bd731cf17c47a24cb1d2b68
PradeepKumar111999 <PradeepKumar8371@gmail.com>
2025-02-01 15:34:03 +0530

    update 2

--- data/index.ts
+++ data/index.ts
@@ -16,7 +16,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "./b1.svg",
     spareImg: "",
   },
   {
@@ -46,8 +46,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
   },
 
   {
@@ -57,8 +57,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
   },
   {
     id: 6,
@@ -77,48 +77,48 @@ export const projects = [
     id: 1,
     title: "Fanmode",
     des: "Fanmode enables remote fans watching games from their homes or mobile phones to actively participate in the games from their locations. Players can experience comprehensive fan support from worldwide audiences in real-time during the game. The services have progressed to enable fans to express support beyond the game and outside the realm of sports, providing new digital tools that bring fans closer to their interests.",
-    img: "/Fanmode.png",
-    iconLists: ["/Html.png","/Css.png","/Figma.png","/Bootstrap.png"],
+    img: "./Fanmode.png",
+    iconLists: ["./Html.png","./Css.png","./Figma.png","./Bootstrap.png"],
     link: "https://www.fanmode.com",
   },
   {
     id: 2,
     title: "MarketSpark",
     des: "MarketSpark leads in offering analog replacement solutions by substituting static and expensive copper telephone lines (POTS) with a managed wireless solution prepared for 5G integration in the enterprise. The MarketSpark Command Center, a cloud-enabled platform, offers comprehensive visibility and absolute control over all wireless services, delivering actionable insights.",
-    img: "/MarketSpark.png",
-    iconLists: ["/Html.png","/Css.png","MaterialUi.png","/ts.svg","/re.svg"],
+    img: "./MarketSpark.png",
+    iconLists: ["./Html.png","./Css.png","MaterialUi.png","./ts.svg","./re.svg"],
     link: "https://www.marketspark.com/product/",
   },
   {
     id: 3,
     title: "Vinayak",
     des: "Vinayak is scaffold service provider company, The site supervisor gives an order, and the manager approves it. After the manager's approval, the storeman processes the order based on stock availability, arranging the driver and vehicle. Once the supervisor receives the material, they perform construction work with it. After completing the construction work, they return the material to the warehouse, where the stock is validated and tracked by the storeman.",
-    img: "/Vinayak.png",
-    iconLists: ["/Html.png","/Css.png","/Js.png","/ts.svg","/re.svg"],
+    img: "./Vinayak.png",
+    iconLists: ["./Html.png","./Css.png","./Js.png","./ts.svg","./re.svg"],
     link: "https://vinayak.com.sg",
   },
   {
     id: 4,
     title: "Arii(Web App)",
     des: "The Students Academic Performance Tracker (Web Client) is an online tool for monitoring students' academic progress. It provides real-time access to grades, attendance, and assignments. User-friendly interfaces and visual aids like charts make data easy to understand. The tool supports personalized learning by highlighting trends and areas for improvement. It enhances communication among teachers, students, and parents, promoting better academic outcomes.",
-    img: "/Arii(web).png",
-    iconLists: ["/Html.png","/Css.png","/Js.png","/ts.svg","/re.svg"],
+    img: "./Arii(web).png",
+    iconLists: ["./Html.png","./Css.png","./Js.png","./ts.svg","./re.svg"],
     link: "https://www.arii.in",
   },
   {
     id:5,
     title: "Arii(Mobile App)",
     des: "The Students Academic Performance Tracker (Mobile Client) is a mobile app designed to monitor students' academic progress. It provides real-time access to grades, attendance, and assignments on-the-go. The app features user-friendly interfaces and visual aids like charts for easy data understanding. By highlighting trends and areas needing improvement, it supports personalized learning. This tool enhances communication between teachers, students, and parents, promoting better academic outcomes",
-    img: "/Arii(mobile).png",
-    iconLists: ["/Html.png","/Css.png","/Js.png","/ts.svg","/re.svg"],
+    img: "./Arii(mobile).png",
+    iconLists: ["./Html.png","./Css.png","./Js.png","./ts.svg","./re.svg"],
     link: "https://play.google.com/store/apps/details?id=com.paabulumdiscoveries.arii&hl=en",
   },
   {
     id:6,
     title: "Confidential Project",
     des: "Due to confidentiality agreements, I am unable to disclose the name, details or specific requirements of the project",
-    img: "/ProductFinder.jpg",
-    iconLists: ["/Html.png","/Css.png","/Js.png","/ts.svg","/re.svg"],
+    img: "./ProductFinder.jpg",
+    iconLists: ["./Html.png","./Css.png","./Js.png","./ts.svg","./re.svg"],
     link: "null",
   },
 ];
@@ -160,32 +160,32 @@ export const companies = [
   {
     id: 1,
     name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    img: "./cloud.svg",
+    nameImg: "./cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
+    img: "./app.svg",
+    nameImg: "./appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
+    img: "./host.svg",
+    nameImg: "./hostName.svg",
   },
   {
     id: 4,
     name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
+    img: "./s.svg",
+    nameImg: "./streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    img: "./dock.svg",
+    nameImg: "./dockerName.svg",
   },
 ];
 
@@ -195,40 +195,40 @@ export const workExperience = [
     title: "Frontend Developer",
     desc: "Successfully delivered multiple high-quality projects, resulting in positive client feedback.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "./exp1.svg",
   },
   {
     id: 2,
     title: "Mobile App Dev",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Capacitor.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "./exp2.svg",
   },
   {
     id: 3,
     title: "Figma Design Expertise",
     desc: "I have experience in Figma, designing user interfaces and creating prototypes.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "./exp3.svg",
   },
   {
     id: 4,
     title: "WordPress development",
     desc: "Currently building a web application using WordPress.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "./exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
     link:"https://github.com/PradeepKumar111999"
   },
   {
     id: 2,
-    img: "/link.svg",
+    img: "./link.svg",
     link:"https://www.linkedin.com/in/pradeep-kumar-904192221"
   },
 ];
